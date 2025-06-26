180dac8e883a301779ba72eba0a8ce2a1735f72e
kavleencodes <kaurkavleen@gmail.com>
2025-06-26 19:30:33 +0530

    built the options page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import SwarifyLogo from './components/SwarifyLogo'
+import Options from './components/Options';
 
 function App() {
+
+  const [showMainContent,setShowMainContent]=useState(false)
+
+  useEffect(()=>{
+    const timer =setTimeout(()=>{
+      setShowMainContent(true);
+    },2500);
+    return ()=>clearTimeout(timer)
+  },[]);
   
 
   return (
     <>
-      <SwarifyLogo/>
+      {/* <SwarifyLogo/> */}
+
+      {!showMainContent ?(
+        <SwarifyLogo/>
+      ):(
+        <Options/>
+      )}
     </>
   )
 }
---
+++ src/components/Options.jsx
@@ -0,0 +1,21 @@
+import styles from './Options.module.css';
+
+export default function Options() {
+  return (
+    <div className={styles.optionsWrapper}>
+      <h2 className={styles.heading}>What would you like to explore?</h2>
+
+      <div className={styles.cardContainer}>
+        <div className={styles.optionCard}>
+          <span role="img" aria-label="artist" className={styles.emoji}>🎤</span>
+          <p className={styles.cardText}>Search by Artist</p>
+        </div>
+
+        <div className={styles.optionCard}>
+          <span role="img" aria-label="song" className={styles.emoji}>🎵</span>
+          <p className={styles.cardText}>Search by Song</p>
+        </div>
+      </div>
+    </div>
+  );
+}
